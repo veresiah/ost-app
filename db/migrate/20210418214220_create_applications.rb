232efd64843d86e94dbb6ea152d1b41e561957aa
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-18 18:17:08 -0400

    modify applications columns

--- db/migrate/20210418214220_create_applications.rb
+++ db/migrate/20210418214220_create_applications.rb
@@ -3,6 +3,11 @@ class CreateApplications < ActiveRecord::Migration[6.1]
     create_table :applications do |t|
       t.integer :user_id
       t.integer :program_id
+      t.string :first_name 
+      t.string :last_name
+      t.string :age
+      t.string :gender
+      t.string :school
       t.string :primary_doctor
       t.string :allergies
       t.string :medical_conditions
