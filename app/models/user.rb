823c36d71452338a0b9a403a30558fd48000e8c0
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-18 18:30:20 -0400

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
     has_many :applications
     has_many :programs, through: :applications 
 
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :username, presence: true, uniqueness: true 
+    validates :password, presence: true
+    validates :email_address, presence: true, uniqueness: true
+    validates :phone_number, length: { is: 10 }
+    validates :street_address, presence: true 
+    validates :city, presence: true 
+    validates :state, presence: true
+    validates :zip_code, presence: true length: { is: 5 }
 end
