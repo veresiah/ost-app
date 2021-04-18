868d6959279d810a1805bef1946fb8c185e867e9
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-18 18:01:20 -0400

    add rows to application migration file

--- db/migrate/20210418214220_create_applications.rb
+++ db/migrate/20210418214220_create_applications.rb
@@ -3,6 +3,13 @@ class CreateApplications < ActiveRecord::Migration[6.1]
     create_table :applications do |t|
       t.integer :user_id
       t.integer :program_id
+      t.string :primary_doctor
+      t.string :allergies
+      t.string :medical_conditions
+      t.string :medications
+      t.string :emergency_contact_name
+      t.string :emergency_contact_relationship
+      t.string :emergency_contact_number
 
       t.timestamps
     end
