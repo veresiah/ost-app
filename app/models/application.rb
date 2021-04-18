9f80cc1358dd69a7c228a7faab7391d89a90ea7c
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-18 18:40:58 -0400

    add application validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,18 @@
 class Application < ApplicationRecord
+    belongs_to :user 
+    belongs_to :program 
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :DOB, presence: true 
+    validates :gender, presence: true 
+    validates :grade, presence: true 
+    validates :school, presence: true  
+    validates :primary_doctor, presence: true 
+    validates :allergies, presence: true
+    validates :medical_conditions, presence: true
+    validates :medications, presence: true
+    validates :emergency_contact_name, presence: true
+    validates :emergency_contact_relationship, presence: true
+    validates :emergency_contact_number, length: { is: 10 }
 end
