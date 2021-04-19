106a0786d09386bc984fb302fe440226463d3337
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-19 09:31:00 -0400

    add index, new and create controller actions

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,16 +1,24 @@
 class ProgramsController < ApplicationController
     def index 
+        @programs = Program.all
     end 
 
-    def new 
+    def new
+        @program = Program.new  
     end 
 
     def create 
+        @program = Program.new(program_params)
+        if @program.save
+            redirect_to programs_path(@program), notice: "Successfully added a new program"
+        else 
+            render "new"
+        end 
     end  
 
     def edit
     end
-    
+
     def show 
     end   
 
