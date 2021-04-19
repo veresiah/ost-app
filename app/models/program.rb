fa62181a25fc35be9369470a6e0a37635a544f8d
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-19 08:47:49 -0400

    add program validations

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,5 +2,10 @@ class Program < ApplicationRecord
     has_many :applications 
     has_many :users, through: :applications 
 
-    
+    validates :name, presence: true
+    validates :agency, presence: true
+    validates :contact_number, length: { is: 10 }
+    validates :grade_level, presence: true 
+    validates :address, presence: true 
+    validates :capacity, presence: true
 end
