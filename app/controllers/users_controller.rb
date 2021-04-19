a4c6a08ef07a8a9e974dcb7a579f51b30f3f0fca
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-19 13:09:09 -0400

    stub out sessions and users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController 
+    def new 
+    end 
+
+    def create
+    end 
+
+    def edit 
+    end 
+
+    def show 
+    end 
+
+    def destroy 
+    end 
+    
 end
