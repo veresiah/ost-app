22b16af3b470440c31b361145964cb2553ed6744
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-19 14:10:41 -0400

    add sessions actions

--- app/controllers/sessionscontroller.rb
+++ app/controllers/sessionscontroller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
     def new 
+        @user = User.new 
     end 
 
     def create
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user), notice: "Login was successful"
+        else 
+            flash[:error] = "Invalid credentials"
+            render "new"
+        end 
     end 
 
     def destroy 
+        session.delete('user_id')
+        redirect_to signin_path, notice: "Logged out successfully"
     end 
 end 
