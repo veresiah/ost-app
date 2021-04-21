cbaf07c1f87a32f79e6a9d56d6ada9da4026f5b4
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-21 14:29:51 -0400

    add user edit, show, update action code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,25 @@ class UsersController < ApplicationController
     end 
 
     def edit 
+        @user = User.find(params[:id])
     end 
 
     def show 
+        @user = User.find(params[:id])
+    end 
+
+    def update 
+        user = User.find(params[:id])
+        if user.update(user_params)
+            redirect_to user_path(user)
+        else 
+            render "edit"
+        end 
     end 
 
     def destroy 
+        @user = User.find(params[:id])
+        @user.destroy 
+        redirect_to '/'
     end 
 end
