e5bbc2daf203a1c1b8390755babdcfaa6fc80e38
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-05-03 17:48:37 -0400

    add sign up route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create' 
   delete '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  
 
   root 'sessions#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
