777858724154d5e74c6f2c6adff30fe27228d509
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-07 16:57:04 -0400

    fix sessions controller path and local variables

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
     end  
 
     def create
-        user = User.find_by(email_address: params[:email_address])
-        if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            redirect_to user_path(user), notice: "Login was successful"
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+            flash[:success] = "Login was successful"
         else 
             flash[:error] = "Invalid credentials"
             render "new"
@@ -14,17 +15,19 @@ class SessionsController < ApplicationController
     end 
 
     def create_using_facebook
-        user = User.find_or_create_by(email: auth['email']) do |u|
+        @user = User.find_or_create_by(email: auth['email']) do |u|
             u.email = auth['info']['email']
             u.password = auth['info']['password']
         end 
-        session[:user_id] = user.id
-        redirect_to user_path(user), notice: "Login with Facebook was successful"
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+        flash[:success] = "Login with Facebook was successful"
     end 
         
     def destroy 
         session.delete :user_id
-        redirect_to login_path, notice: "Logged out successfully"
+        redirect_to login_path
+        flash[:notice] = "Logged out successfully"
     end 
 
     private
