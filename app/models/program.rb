bf909583c1f9833d52d23640f9d3a23011de458a
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-12 14:37:53 -0400

    fix validations for programs

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,17 +2,8 @@ class Program < ApplicationRecord
     has_many :applications 
     has_many :users, through: :applications 
 
-    validates :name, presence: true
-    validates :agency, presence: true
+    validates :name, :agency, :grade_level, :borough, :address, presence: true
     validates :contact_number, length: { is: 10 }
-    validates :grade_level, presence: true 
-    validates :address, presence: true 
-    validates :capacity, presence: true
-    #validates :street_address, presence: true 
-    #validates :city, presence: true 
-    #validates :state, presence: true
-    #validates :zip_code, presence: true, length: { is: 5 }
-
     #scope :filter_by_agency -> {where("agency = ?", params[:agency])}
     #scope :filter_by_name -> {where("name = ?", params[:name])}
     #scope :filter_by_grade_level -> {where("grade_level = ?", params[:grade_level])}
