03e9dfb3f19dd1cf916785428af1238c3e14f8d8
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-12 14:39:13 -0400

    refractor user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
     has_many :applications
     has_many :programs, through: :applications 
 
-    validates :password, presence: true
+    validates :name, :password, presence: true
     validates :email, presence: true, uniqueness: true
-    validates :name, presence: true
 
     def self.from_omniauth(response)
         User.find_or_create_by(uid: response['uid'], provider: response['provider']) do |u|
